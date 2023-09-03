64a632dd9df7ede68b0ee894e752dc264ef1704a
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-09-03 17:06:17 -0400

    insallted cors package and made changes to server.js to resolve cors error

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,20 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+//setup cors
+const cors = require('cors');
+app.use(cors());
 const io = new Server(server,
-  { cors: { origin: 'http://localhost:5173' } }
+  { cors: { origin: 'http://localhost:5173/' } }
 );
 
 
 io.on('connection', socket => {
   console.log('a user connected');
 });
+
+
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
