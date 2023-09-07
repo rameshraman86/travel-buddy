7f70c4225be20961622e4b0ee387d3df774b758f
Natasha Chiu <chiuchiu07@gmail.com>
2023-09-07 15:31:14 -0400

    google api places

--- backend/routes/google-api.js
+++ backend/routes/google-api.js
@@ -5,12 +5,43 @@ const { Client } = require("@googlemaps/google-maps-services-js");
 
 const client = new Client({});
 
+async function getPlaceInfo(query) {
+  const encodedQuery = encodeURIComponent(query); // Encode the query parameter
+  let textSearch = `https://maps.googleapis.com/maps/api/place/textsearch/json`;
+  textSearch += `?location=42.3675294%2C-71.186966&query=${encodedQuery}`;
+  textSearch += `&radius=10000`;
+  textSearch += `&key=AIzaSyDlKwuiOlQJfWG0n7i-yAv3KuBjsqmCXuI`;
+  // console.log(process.env.VITE_GOOGLE_MAPS_API_KEY);
+
+  try {
+    const response = await axios.get(textSearch);
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+router.get('/places', async (req, res) => {
+  try {
+    console.log('api hit');
+    const placeInfo = await getPlaceInfo(req.body);
+    res.send(placeInfo);
+
+  } catch (error) {
+    res.json({ error_fetch_from_google_api: error.message });
+  }
+});
+
+
 router.get("/", async (req, res) => {
   client
     .elevation({
       params: {
-        locations: [{ lat: 45, lng: -110 }],
-        key: process.env.VITE_GOOGLE_MAPS_API_KEY,
+        address: "123 main street",
+        location: [{ lat: 42.3675294, lng: -71.186966 }],
+        key: "AIzaSyDlKwuiOlQJfWG0n7i-yAv3KuBjsqmCXuI",
       },
       timeout: 1000, // milliseconds
     })
@@ -20,45 +51,6 @@ router.get("/", async (req, res) => {
     .catch((e) => {
       console.log(e.response.data.error_message);
     });
-});
-
-async function getPlaceInfo() {
-  // const encodedQuery = encodeURIComponent(query); // Encode the query parameter
-
-  const textSearch = `https://maps.googleapis.com/maps/api/place/textsearch/json
-  ?location=42.3675294%2C-71.186966
-  &query=123%20main%20street
-  &radius=10000
-  &key=${process.env.VITE_GOOGLE_MAPS_API_KEY}`;
-  // console.log(process.env.VITE_GOOGLE_MAPS_API_KEY);
-
-  try {
-    const response = await axios.get(textSearch);
-
-    if (response.status !== 200) {
-      throw new Error(`Google Places API request failed with status: ${response.status}`);
-    }
-    console.log(response);
-
-    const data = await response.json();
-    return data;
-
-  } catch (error) {
-    throw error;
-  }
-
-
-}
-
-router.get('/places', async (req, res) => {
-  try {
-    console.log('api hit');
-    const placeInfo = await getPlaceInfo();
-    res.send(placeInfo);
-
-  } catch (error) {
-    res.status(500).json({ error_fetch_from_google_api: error.message });
-  }
 });
 
 module.exports = router;
--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -16,5 +16,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+
+    "react/prop-types": 0
+
   },
-}
+};
