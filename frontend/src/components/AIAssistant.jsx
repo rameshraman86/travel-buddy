926dd3a63f69a8129c278410ed0f900dfc4c6324
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-09-07 08:05:23 -0400

    installed gpt dependencies

---
+++ frontend/src/components/AIAssistant.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function AIAssistant({ id }) {
+
+  return (
+
+    <h2>Hi! I am your AI Assistant</h2>
+    
+  );
+
+}
--- frontend/src/components/TripDetails.jsx
+++ frontend/src/components/TripDetails.jsx
@@ -1,8 +1,9 @@
-import React  from "react";
+import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Itineraries from "./Itineraries";
 import Messages from "./Messages";
 import Chat from './Chat';
+import AIAssistant from "./AIAssistant";
 
 
 export default function TripDetails() {
@@ -12,8 +13,9 @@ export default function TripDetails() {
       <h1>Trip {id} details</h1>
       <Itineraries tripID={id} />
       <Messages tripID={id} />
-      <Chat/>
+      <Chat />
       <h2>Map</h2>
+      <AIAssistant tripID={id} />
     </>
   );
 }
