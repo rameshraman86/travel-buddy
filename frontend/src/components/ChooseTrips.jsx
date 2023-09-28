c841376eba8659129fa04b82a703d84db26cc0b6
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-09-27 21:22:36 -0400

    feat: load trip details in choose trips page

--- frontend/src/components/ChooseTrips.jsx
+++ frontend/src/components/ChooseTrips.jsx
@@ -12,23 +12,41 @@ export default function ChooseTrips({ email, setEmail }) {
 
     async function fetchData() {
       const allTrips = await axios.get(`http://localhost:8080/api/trips/get-all-trips-of-user/${email}`);
-      console.log(allTrips.data);
-
+      setTrips(allTrips.data);
     }
     fetchData();
   }, []);
 
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit' });
+  }
+
+
   return (
     <>
       <h1>Welcome Back {email}. Choose from your trips below to continue planning or create a new trip.</h1>
-      <ChooseTripsItems
-        email={email}
-        setEmail={setEmail}
-        trips={trips}
-        setTrips={setTrips}
-      />
-
-      <h1>Click <Link className="text-amber-600" to='/new'>here</Link> to create a new Trip.</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Trip</th>
+            <th>Location</th>
+            <th>Start</th>
+            <th>End</th>
+          </tr>
+        </thead>
+        <tbody>
+          {trips.map(trip => (
+            <tr key={trip.trip_url}>
+              <td><a href={trip.trip_url + '/details'}>{trip.trip_name}</a></td>
+              <td>{trip.trip_location}</td>
+              <td>{formatDate(trip.start_date)}</td>
+              <td>{formatDate(trip.end_date)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h1> <Link className="text-amber-600" to='/new'>Create a new Trip!</Link></h1>
 
     </>
   );
