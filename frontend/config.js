d73af48fc8b842ed6fded05517eca1a3deda47c2
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-09-29 14:56:15 -0400

    redeploying

--- frontend/config.js
+++ frontend/config.js
@@ -1,8 +1,8 @@
 const apiConfig = {
   production: 'https://travelbuddy-api.onrender.com',
   development: 'http://localhost:8080',
-  // app_production: 'https://travel-buddy-x9ue.onrender.com',
-  app_production: 'https://travel-buddy-sepia-tau.vercel.app',
+  app_production_render: 'https://travel-buddy-x9ue.onrender.com',
+  app_production_vercel: 'https://travel-buddy-sepia-tau.vercel.app',
   app_development: 'http://localhost:5173',
 };
 
