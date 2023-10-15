1f8c2887c05ad87474bb84559714dd96c5ed8dc7
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-10-15 05:17:34 -0400

    copy the trip url with copy button or close the modal

--- frontend/src/components/TripDetails.jsx
+++ frontend/src/components/TripDetails.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import Itineraries from "./Itineraries";
@@ -30,6 +30,7 @@ export default function TripDetails(props) {
   const [suggestedPlaces, setSuggestedPlaces] = useState(undefined);
   const [mapRef, setMapRef] = useState(undefined);
   const [selectedPlace, setSelectedPlace] = useState(undefined);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const navigate = useNavigate();
 
@@ -126,7 +127,6 @@ export default function TripDetails(props) {
   };
 
 
-
   const handleMarkerClick = (place) => {
     mapRef?.setZoom(14);
     mapRef?.panTo({ lat: place.lat, lng: place.lng });
@@ -138,9 +138,22 @@ export default function TripDetails(props) {
 
   const handleShareTripButtonClick = (event) => {
     event.preventDefault();
-    console.log(`Share trip: ${app_url}/${id}`);
+    setModalIsOpen(!modalIsOpen);
   };
 
+
+  async function copyToClipboard() {
+    try {
+      const tripURLTextBox = document.getElementById('tripLink');
+      await navigator.clipboard.writeText(tripURLTextBox.placeholder);
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
 
 
   return (
@@ -165,9 +178,29 @@ export default function TripDetails(props) {
           <form onSubmit={handleShareTripButtonClick}>
             <h1 className="text-2xl font-bold text-gray-800 gap-14">{tripName}</h1>
             <button className=" bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold py-1 px-2.5 rounded-md inline-flex items-center text-start">
-              Invite others
+              +Invite Friends
             </button>
           </form>
+          {modalIsOpen &&
+
+            //     <svg fill="#d97706" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+            //       <path d="M0 25.472q0 2.368 1.664 4.032t4.032 1.664h18.944q2.336 0 4-1.664t1.664-4.032v-8.192l-3.776 3.168v5.024q0 0.8-0.544 1.344t-1.344 0.576h-18.944q-0.8 0-1.344-0.576t-0.544-1.344v-18.944q0-0.768 0.544-1.344t-1.344-0.544h9.472v-3.776h-9.472q-2.368 0-4.032 1.664t-1.664 4v18.944zM5.696 19.808q0 2.752 1.088 5.28 0.512-2.944 2.24-5.344t4.288-3.872 5.632-1.664v5.6l11.36-9.472-11.36-9.472v5.664q-2.688 0-5.152 1.056t-4.224 2.848-2.848 4.224-1.024 5.152zM32 22.080v0 0 0z"></path>
+            //     </svg>
+            <div className="">
+              <div className="font-medium leading-10 text-gray-900 flex items-center gap-5">Invite your friends to collaborate on this trip by sharing this unique link.</div>
+              <div className="flex items-center gap-2 mb-4">
+                <input
+                  type="text"
+                  placeholder={`${app_url}/${id}`}
+                  readOnly
+                  id="tripLink"
+                />
+                <button onClick={copyToClipboard} className='rounded-xl border border-transparent bg-amber-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-amber-700 focus:outline-none'>Copy</button>
+                <button onClick={closeModal} className="inline-flex justify-center px-4 py-2 text-sm font-semibold  text-white bg-gray-600 border border-transparent rounded-xl hover:bg-gray-700 duration-300" >Close</button>
+              </div>
+            </div>
+          }
+
           <div className="flex items-center">
             <span className="text-lg text-gray-600 font-extrabold mx-0.5">{location}</span>
             <span className="w-1.5 h-1.5 mx-1.5 bg-gray-600 rounded-full dark:bg-gray-400"></span>
